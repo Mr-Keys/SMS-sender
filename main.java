377131c5c6a788ea2de532bb6202be7d5ea118d0
Mr-Keys <31483590+Mr-Keys@users.noreply.github.com>
2018-05-17 11:47:29 +0530

    Create main.java

---
+++ main.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+ 
+public class sendSMS {
+	public String sendSms(apiKey,message,sender,numbers) {
+		try {
+			// Construct data
+			apiKey = "apikey=" + "yourapiKey";
+			message = "&message=" + "This is your message";
+			sender = "&sender=" + "Jims Autos";
+			numbers = "&numbers=" + "447123456789";
+			
+			// Send data
+			HttpURLConnection conn = (HttpURLConnection) new URL("https://api.txtlocal.com/send/?").openConnection();
+			String data = apiKey + numbers + message + sender;
+			conn.setDoOutput(true);
+			conn.setRequestMethod("POST");
+			conn.setRequestProperty("Content-Length", Integer.toString(data.length()));
+			conn.getOutputStream().write(data.getBytes("UTF-8"));
+			final BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+			final StringBuffer stringBuffer = new StringBuffer();
+			String line;
+			while ((line = rd.readLine()) != null) {
+				stringBuffer.append(line);
+			}
+			rd.close();
+			
+			return stringBuffer.toString();
+		} catch (Exception e) {
+			System.out.println("Error SMS "+e);
+			return "Error "+e;
+		}
+	}
+}
